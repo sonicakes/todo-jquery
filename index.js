1642ccefa5c70902dbd5cf340d7c8505408c12f6
Sonia Parker <sonia.parker@ping-works.com.au>
2021-02-08 13:47:56 +1100

    add logic to removing items from the list

--- index.js
+++ index.js
@@ -20,25 +20,32 @@ function doStuff() {
     if (($input).val() !== "") {
         let newTodo = $input.val();
         console.log(newTodo);
-
-
         todosArray.push(newTodo);
         console.log(todosArray);
         
-        $('#output').append( "<li id='" + newTodo + "'>" + newTodo + "<span><button class='removeTodo' id='" + newTodo + "'>Remove</button></span></li>" );
+        $('#output').append( "<li id='" + newTodo + "'>" + newTodo + "<button class='removeTodo'>Remove</button><button class='markTodo'>Mark as Complete</button></li>" );
+        
+        
+            //2. remove it from todosArray (by clicking the 'Remove' button)
+
+        $(".removeTodo").on( "click", function() {
+                //1. find the element that needs to be removed
+
+            let idRemoval = $( this ).parent().attr('id');
+            console.log(idRemoval);
+            todosArray.splice(todosArray.indexOf(idRemoval), 1)
+            console.log(todosArray);
+                //3. remove it from the html list output
+
+            $( this ).parent().remove();
+          });
+    
     }
-  
+    $input.val("");
   }
   //clear the value when the mouse is not inside the input
   $input.focusout(function() {
    
     $input.val("");
-
-    //remove the item from the list
-    //1. find the element that needs to be removed
-    //2. remove it from todosArray (by clicking the 'Remove' button)
-    //3. cross it out in the html list output
-});
-   
-           
+});          
 });
