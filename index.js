7c2d8b6c0ed60ae04d6fd6cefdc525b8a75cb598
Sonia Parker <sonia.parker@ping-works.com.au>
2021-02-08 15:02:21 +1100

    mark as complete logic and updating the items remaining

--- index.js
+++ index.js
@@ -22,23 +22,88 @@ function doStuff() {
         console.log(newTodo);
         todosArray.push(newTodo);
         console.log(todosArray);
+        console.log(todosArray.length);
+
+        if (todosArray.length === 1) {
+            $('#itemsLeft').text(todosArray.length + " item left");
+  
+        }else {
+            $('#itemsLeft').text(todosArray.length + " items left");
+
+        }
         
-        $('#output').append( "<li id='" + newTodo + "'>" + newTodo + "<button class='removeTodo'>Remove</button><button class='markTodo'>Mark as Complete</button></li>" );
+        $('#output').append("<label id='" + newTodo + "' class='list-group-item'><input class='form-check-input me-1' type='checkbox' value=''>"  + newTodo + "<span><button class='removeTodo btn btn-danger'>Remove</button><button class='markTodo btn btn-success'>Mark as Complete</button> </span></label>");
         
-        
-            //2. remove it from todosArray (by clicking the 'Remove' button)
+            // remove it from todosArray (by clicking the 'Remove' button)
 
         $(".removeTodo").on( "click", function() {
-                //1. find the element that needs to be removed
+                 //find the element that needs to be removed
 
-            let idRemoval = $( this ).parent().attr('id');
+            let idRemoval = $( this ).closest('.list-group-item').attr('id');
             console.log(idRemoval);
-            todosArray.splice(todosArray.indexOf(idRemoval), 1)
-            console.log(todosArray);
-                //3. remove it from the html list output
+            if (todosArray.indexOf(idRemoval) !== -1 ) {
+                todosArray.splice(todosArray.indexOf(idRemoval), 1);
+                console.log(todosArray);
+                if (todosArray.length === 1) {
+                    $('#itemsLeft').text(todosArray.length + " item");
+          
+                }else if (todosArray.length <= 0 ) {
+                        $('#itemsLeft').text("no items in your todo");
+                }
+                else {
+                    $('#itemsLeft').text(todosArray.length + " items");
+        
+                }
+                    // remove it from the html list output
+    
+                $( this ).closest('.list-group-item').remove();
+            } 
 
-            $( this ).parent().remove();
+          
+
+
           });
+
+          $(".markTodo").on( "click", function() {
+           // find the element that needs to be marked
+
+       let idMarked = $( this ).closest('.list-group-item').attr('id');;
+       console.log(idMarked);
+       console.log(todosArray);
+       if (todosArray.indexOf(idMarked) !== -1 ) {
+        todosArray.splice(todosArray.indexOf(idMarked), 1);
+        console.log(todosArray);
+        if (todosArray.length === 1) {
+            $('#itemsLeft').text(todosArray.length + " item");
+  
+        }else if (todosArray.length <= 0 ) {
+                $('#itemsLeft').text("Wow! You completed all items in your list! What a champ!");
+                $('.gif').show();
+        }
+        else {
+            $('#itemsLeft').text(todosArray.length + " items");
+
+        }
+    }
+
+           // remove it from the html list output
+        $( this ).closest('label').css("text-decoration", "line-through");
+    
+       $( this ).closest('label').addClass('complete');
+
+       
+     });
+
+     $(".clear-completed").on( "click", function() {
+        $("label.complete").remove();
+        todosArray.splice(todosArray.indexOf(idMarked), 1)
+        console.log(todosArray);
+    
+
+     });
+
+
+
     
     }
     $input.val("");
@@ -47,5 +112,7 @@ function doStuff() {
   $input.focusout(function() {
    
     $input.val("");
-});          
+});
+
+
 });
